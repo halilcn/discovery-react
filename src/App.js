68bfd5068853f51e63bfb996eb9af9cd79f1216b
Halil CAN <halilc.2001@gmail.com>
2022-01-21 19:36:22 +0300

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -11,21 +11,23 @@ import { UserDetail } from './components/dashboard/UserDetail';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/dashboard/home" component={<Home/>}/>
-        <Route path="/dashboard/users" component={<UserList/>}/>
-        <Route path="/dashboard/users/:userId" component={<UserDetail/>}/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/dashboard/home" component={<Home/>}/>
+          <Route path="/dashboard/users" component={<UserList/>}/>
+          <Route path="/dashboard/users/:userId" component={<UserDetail/>}/>
 
 
-        <Route path="/test/" element={<Test/>}>
-          <Route path="2" element={<Test2/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/test/" element={<Test/>}>
+            <Route path="2" element={<Test2/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/general/Login.js
+++ src/components/general/Login.js
@@ -1,13 +1,27 @@
 import { Link } from 'react-router-dom';
-import '../../styles/login.css';
 
 export const Login = (props) => {
   return (
-    <div>
-      <div className="test">
-        test denee
+    <div className="bg-gray-300 p-6">
+      <div className="bg-white w-full lg:w-1/3 mx-auto rounded-lg px-4 py-4 shadow-lg">
+        <input type="text"
+               placeholder="Email or Phone Number"
+               className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"/>
+        <input type="text" placeholder="Password"
+               className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"/>
+        <div className="text-white py-3 rounded-lg w-full font-bold text-xl tracking-wider">Log
+          In
+        </div>
+        <div className="flex justify-center my-4">
+          <a className="text-blue-500 text-sm" href="#">Forgot account?</a>
+        </div>
+        <div className="flex justify-center my-6">
+          <div className="text-white h-12 rounded px-6 font-bold">Create
+            new Account
+          </div>
+        </div>
+        <Link to="/register">register</Link>
       </div>
-      <Link to="/register">register</Link>
     </div>
   );
 };
